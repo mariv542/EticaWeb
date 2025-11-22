03ee27b69dadec6c605d9c6f27a90b24eb12ec01
mariv542 <marielvy542@gmail.com>
2025-11-22 14:42:28 -0300

    cambios de rutas

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Servir archivos estáticos desde /src/public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal → Renderiza un HTML desde /src/views
+// Ruta principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
